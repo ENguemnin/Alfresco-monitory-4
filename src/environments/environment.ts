f726231cd4455fcce04f2f5548d2704861b92751
AlexNGUEMNIN <alexnguemnin@gmail.com>
2025-07-29 00:01:19 +0100

    Update et mise a jour du projet de monitoring4

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  api2Url: 'http://localhost:8082/api', // Example API URL for development
+  apiUrl: 'http://localhost:8083/api', // Example API URL for development
+  websocket2Url: 'ws://localhost:8082/ws',
+  websocketUrl: 'http://localhost:8083/ws',
+  BASE_URL: "http://192.168.30.111:8080"
+};
